928318579bc39cb7e06210819755af13086f753c
achtergarde <nils.achtergarde@gbv.de>
2017-11-26 18:54:05 +0100

    restructured dockerfile and copy new build to host

--- create.sh
+++ create.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
- docker build -t gpg4usb-linux-32bit -f dockerfile_devenv_gpg4usb_linux_32bit .
- docker build -t gpg4usb-linux-64bit -f dockerfile_devenv_gpg4usb_linux_64bit .
+docker build -t gpg4usb-linux-32bit -f dockerfile_devenv_gpg4usb_linux_32bit .
+docker build -t gpg4usb-linux-64bit -f dockerfile_devenv_gpg4usb_linux_64bit .
+
+# Copy new build to /tmp/gpg4usb/gpg4usb_daily.zip on host
+mkdir /tmp/gpg4usb
+docker run -v /tmp/gpg4usb:/out gpg4usb-linux-64bit:latest /bin/cp /opt/gpg4usb_daily.zip /out
